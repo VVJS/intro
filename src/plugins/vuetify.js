644245c2c8226c8adfcc6a8d248026d822a1a858
Amrit Kahlon <amrit@hockey-community.com>
2018-06-15 17:24:50 -0400

    style: Added decath colors to vuetify theme 💎

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,12 +4,12 @@ import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify, {
     theme: {
-        primary: '#ee44aa',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+        primary: '#0082C3',
+        accent: '#FFEA28',
+        secondary: '#242424',
+        info: '#0279D7',
+        warning: '#B71C1C',
+        error: '#B71C1C',
+        success: '#4CAf50'
     }
 })
