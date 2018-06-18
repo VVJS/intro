bc89c188bdf06065d7c9f7f4beb086135bcf827a
Amrit Kahlon <amrit@hockey-community.com>
2018-06-18 15:47:37 -0400

    feat: Fix with moustache templates ✨

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@
         </v-navigation-drawer>
         <v-toolbar app>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-toolbar-title>
+                {{ title }}
+            </v-toolbar-title>
             <v-spacer></v-spacer>
         </v-toolbar>
         <v-content>
