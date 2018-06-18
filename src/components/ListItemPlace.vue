0af87c036762377c62896075dbe0da680ecaf013
Amrit Kahlon <amrit@hockey-community.com>
2018-06-18 19:09:51 -0400

    feat: Added new component for listing ✨

---
+++ src/components/ListItemPlace.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-flex class="px-1" xs12 md6>
+        <v-list-tile avatar>
+            <v-list-tile-avatar>
+                <img :src="getPhotoUrl(place)">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+                <v-list-tile-title class="capitalize title">
+                    {{ place.properties.name }}
+                </v-list-tile-title>
+            </v-list-tile-content>
+        </v-list-tile>
+    </v-flex>
+</template>
+
+<script>
+export default {
+    name: 'PlaceListItem',
+    props: ['place'],
+    methods: {
+        getPhotoUrl(place) {
+            const apiKey = 'AIzaSyAgGwSfZA0eGjv1-Hhncd5SZImxYbgOPb'
+            const backup = 'https://hockeycommunity.com/explore/static/images/explore/outdoor-sml.jpg'
+            const ref = place.properties.activities[0].photo_reference
+            return ref
+                ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photoreference=${ref}&key=${apiKey}`
+                : backup
+        }
+    }
+}
+</script>
+
+<style scoped>
+/deep/ .list__tile {
+    border-radius: 5px;
+    border: 1px solid #dfdfdf;
+    margin-bottom: 8px;
+}
+/deep/ .list__tile:hover {
+    background-color: #0082c385;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,32 @@
 <template>
     <v-container fluid>
         <v-slide-y-transition mode="out-in">
-            <v-layout column align-center>
-                <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-                <blockquote>
-                    &#8220;First, solve the problem. Then, write the code.&#8221;
-                    <footer>
-                        <small>
-                            <em>&mdash;John Johnson</em>
-                        </small>
-                    </footer>
-                </blockquote>
+            <v-layout row wrap>
+                <list-item-place :place="place" v-for="place in sportPlaces.slice(0, 20)" :key="place.properties.uuid"></list-item-place>
             </v-layout>
         </v-slide-y-transition>
     </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-    font-weight: normal;
+<script>
+import axios from 'axios'
+import listItemPlace from '@/components/ListItemPlace.vue'
+
+export default {
+    name: 'App',
+    components: {
+        listItemPlace
+    },
+    data() {
+        return {
+            sportPlaces: []
+        }
+    },
+    async mounted() {
+        const { data } = await axios.get(
+            'https://sportplaces-api.herokuapp.com/api/v1/places?radius=99&origin=-73.5826985,45.5119864'
+        )
+        this.sportPlaces = data.data.features
+    }
 }
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
-}
-</style>
+</script>
